c95cf703668cdfe20289e63060eb1ea323e073cb
JULIO <julio@gestao>
2023-09-13 20:35:07 -0300

    Projeto da festa

---
+++ src/main/java/br/gov/sp/etec/gestaofesta/controller/LoginRepository.java
@@ -0,0 +1,15 @@
+package br.gov.sp.etec.gestaofesta.controller;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.gov.sp.etec.gestaofesta.model.Login;
+
+@Repository 
+public interface LoginRepository extends JpaRepository<Login, Long> {
+	
+	Login findByEmail(String Email);
+	
+	
+
+}
